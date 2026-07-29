171390f24a60ff1c5e4b65c49319f0c6b6a99ad2
Matt Blindbard <62107349+Blindbard666@users.noreply.github.com>
2026-07-30 00:46:22 +0200

    Add files via upload

---
+++ frasi.js
@@ -0,0 +1,48 @@
+const FRASI = {
+
+    facile: [
+
+        "Il gatto dorme",
+        "La luna splende",
+        "Oggi piove",
+        "La casa è grande",
+        "Il cane corre",
+        "Bevo un caffè",
+        "La neve cade",
+        "Il sole tramonta",
+        "Il pane è caldo",
+        "La porta è aperta"
+
+    ],
+
+    media: [
+
+        "La pazienza è la virtù dei forti",
+        "Chi va piano va sano e va lontano",
+        "Il tempo è il bene più prezioso",
+        "Meglio tardi che mai",
+        "Le parole hanno un grande potere",
+        "Ogni giorno è una nuova occasione",
+        "La fortuna aiuta gli audaci",
+        "L'unione fa la forza",
+        "Non rimandare a domani",
+        "Ogni scelta ha una conseguenza"
+
+    ],
+
+    difficile: [
+
+        "Non c'è cosa più divina che giocare con Yasmina",
+        "La conoscenza è l'unica ricchezza che aumenta quando viene condivisa",
+        "Ogni grande viaggio comincia con un piccolo passo",
+        "La fantasia è più importante della conoscenza",
+        "Le difficoltà preparano spesso persone comuni a destini straordinari",
+        "Il successo è la somma di piccoli sforzi ripetuti giorno dopo giorno",
+        "La curiosità è il motore di ogni scoperta",
+        "Il coraggio non è l'assenza della paura ma la capacità di affrontarla",
+        "Le opportunità non accadono le crei tu",
+        "La perseveranza trasforma gli ostacoli in traguardi"
+
+    ]
+
+};
